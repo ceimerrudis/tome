885722bfc54ccb6b68f07adebd53d62350a3fa8f
Rūdolfs Ceimers <ceimerrudis@gmail.com>
2026-09-22 14:07:05 +0300

    Added port 443 to docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /tome/dynamic/tome
 docker build -t tome:latest .
-docker run -d --name tome  -p 80:80 -v "/tome/persistent:/var/www/html/data" tome
+docker run -d --name tome -p443:443 -p 80:80 -v "/tome/persistent:/var/www/html/data" tome
 
