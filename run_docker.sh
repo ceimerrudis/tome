91afaeb1388c7e184d17b5c6aa2648bc7692c8a7
Rūdolfs Ceimers <ceimerrudis@gmail.com>
2026-09-22 14:10:20 +0300

    added container stop

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd /tome/dynamic/tome
+
+docker stop "tome" 2>/dev/null || true
+docker rm "tome" 2>/dev/null || true
+
 docker build -t tome:latest .
 docker run -d --name tome -p443:443 -p 80:80 -v "/tome/persistent:/var/www/html/data" tome
 
