c50125c71db964b5f795fd45fbd1995281c89a02
Rūdolfs Ceimers <ceimerrudis@gmail.com>
2026-09-25 19:03:52 +0300

    ficxed form

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<form action="/upload.php">
+<form method="POST" enctype="multipart/form-data" action="/upload.php">
   <input type="file" id="myFile" name="myFile">
   <input type="submit">
 </form>
