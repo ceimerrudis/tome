44f57de6c6f01f64007dc562eed3493eb9440e93
Rūdolfs Ceimers <ceimerrudis@gmail.com>
2026-09-25 18:55:26 +0300

    did stuff

--- src/upload.php
+++ src/upload.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
+<html>
+<body>
 <?php
 
-	$target_dir = "/tome/persistent/";
+	$target_dir = "/var/www/tome/data/";
 	$target_file = $target_dir . "a_";
 	$uploadOk = 1;
-	
-	echo $_SERVER;
-	echo $_FILES;
-	
+
+    echo "begin";
     if(isset($_FILES["myFile"]) && 
     $_FILES["myFile"]["error"] == 0) {
 
+    echo "inif";
         $file_name = $target_file . $_FILES["fileToUpload"]["name"];
     
+    echo "fname_got";
         if (file_exists($target_dir . $file_name)) {
     
+    echo "fex";
         }        
         else {
             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
